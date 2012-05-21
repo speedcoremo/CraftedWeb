bbb2e85dfbb73576aacaa3af7637e420adb04dd8
Anton Hedling <crafteddev@hotmail.com>
2012-05-21 10:51:34 +0200

    Just added some dots to the Username placeholder

--- modules/login.php
+++ modules/login.php
@@ -7,7 +7,7 @@
 	 <div class="box_one_title">Account Management</div> 
          <form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post">
          <fieldset style="border: none; margin: 0; padding: 0;">
-             <input type="text" placeholder="Username" name="login_username" class="login_input" /><br/>
+             <input type="text" placeholder="Username..." name="login_username" class="login_input" /><br/>
              <input type="password" placeholder="Password..." name="login_password" class="login_input" style="margin-top: -1px;" /><br/>
              <input type="submit" value="Log In" name="login" style="margin-top: 4px;" /> 
              <input type="checkbox" name="login_remember" checked="checked"/> Remember me
